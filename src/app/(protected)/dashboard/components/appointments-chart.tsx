7ff58e57219e80905af41cd1080dff4e9bbe83ed
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-06-02 11:57:37 -0300

    feat: add appointments charts

---
+++ src/app/(protected)/dashboard/components/appointments-chart.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import dayjs from "dayjs";
+import { DollarSign } from "lucide-react";
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { formatCurrencyInCents } from "@/helpers/currency";
+
+export const description = "An area chart with gradient fill";
+
+interface RevenueChartProps {
+  dailyAppointmentsData: {
+    date: string;
+    appointments: number;
+    revenue: number;
+  }[];
+}
+
+const AppointmentsChart = ({ dailyAppointmentsData }: RevenueChartProps) => {
+  const chartDays = Array.from({ length: 21 }).map((_item, index) =>
+    dayjs()
+      .subtract(10 - index, "days")
+      .format("YYYY-MM-DD"),
+  );
+
+  const chartData = chartDays.map((day) => {
+    const dayData = dailyAppointmentsData.find((d) => d.date === day);
+    return {
+      date: day,
+      appointments: dayData?.appointments || 0,
+      revenue: dayData?.revenue || 0,
+    };
+  });
+
+  const chartConfig = {
+    appointments: {
+      label: "Agendamentos",
+      color: "#0B68F7",
+    },
+    revenue: {
+      label: "Faturamento",
+      color: "#10B981",
+    },
+  } satisfies ChartConfig;
+
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center gap-2">
+        <DollarSign />
+        <CardTitle>Agendamentos e Faturamento</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig} className="min-h-[200px]">
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              top: 20,
+              left: 20,
+              right: 30,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={10}
+            />
+            <YAxis
+              yAxisId="left"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+            />
+            <YAxis
+              yAxisId="right"
+              orientation="right"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => formatCurrencyInCents(value)}
+            />
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  formatter={(value, name) => {
+                    if (name === "revenue") {
+                      return (
+                        <>
+                          <div className="h-3 w-3 rounded bg-[#10B981]" />
+                          <span className="text-muted-foreground">
+                            Faturamento:
+                          </span>
+                          <span className="font-semibold">
+                            {formatCurrencyInCents(Number(value))}
+                          </span>
+                        </>
+                      );
+                    }
+                    return (
+                      <>
+                        <div className="h-3 w-3 rounded bg-[#0B68F7]" />
+                        <span className="text-muted-foreground">
+                          Agendamentos:
+                        </span>
+                        <span className="font-semibold">{value}</span>
+                      </>
+                    );
+                  }}
+                  labelFormatter={(label, payload) => {
+                    if (payload && payload[0]) {
+                      return dayjs(payload[0].payload?.fullDate).format(
+                        "DD/MM/YYYY (dddd)",
+                      );
+                    }
+                    return label;
+                  }}
+                />
+              }
+            />
+            <Area
+              yAxisId="left"
+              dataKey="appointments"
+              type="monotone"
+              fill="var(--color-appointments)"
+              stroke="var(--color-appointments)"
+              fillOpacity={0.2}
+              strokeWidth={2}
+            />
+            <Area
+              yAxisId="right"
+              dataKey="revenue"
+              type="monotone"
+              fill="var(--color-revenue)"
+              stroke="var(--color-revenue)"
+              fillOpacity={0.2}
+              strokeWidth={2}
+            />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default AppointmentsChart;
